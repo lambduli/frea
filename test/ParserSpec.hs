a1032ec2592e299de9644d996d9b8a51f09c0341
Taskkill <johny95wil@gmail.com>
2021-03-10 23:00:26 +0100

    test let infix function declaration

--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -61,6 +61,10 @@ spec = describe "Test the parser" $ do
     "let a + b = b in x" <=>
       Let "+" (Lam "a" (Lam "b" (Var "b"))) (Var "x")
 
+  it "Parses a function in infix let" $ do
+    "let a `plus` b = b in x" <=>
+      Let "plus" (Lam "a" (Lam "b" (Var "b"))) (Var "x")
+
   it "Parses a multi let rec" $ do
     "let n = 23 rec f m = m in (f n)" <=>
       Let "n" (Lit (LitInt 23)) (Let "f" (Fix (Lam "f" (Lam "m" (Var "m"))) ) (App (Var "f") (Var "n")))
