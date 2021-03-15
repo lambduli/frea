70879c9ad1f090a0fd17d8d371bdfc2c8f4e3ed9
Taskkill <johny95wil@gmail.com>
2021-03-15 17:30:32 +0100

    move EvaluationError and add Thunk Value - unevaluated expression

--- src/Interpreter/Value.hs
+++ src/Interpreter/Value.hs
@@ -18,7 +18,7 @@ data Value
   | Lam String Expression Env
   | Tuple [Value]
   | List [Value]
-  | Thunk (() -> Value)
+  | Thunk (() -> Either EvaluationError Value)
 
 
 instance Show Value where
@@ -26,9 +26,29 @@ instance Show Value where
   show (Lit lit) = show lit
   show (Lam par body env) = "<lambda>"
   show (Tuple values) = "(" ++ intercalate ", " (map show values) ++ ")"
-  -- show (List vals@(Lit (LitChar c) : _))
-  --   = "\"" ++ intercalate "" (map show vals) ++ "\""
   show (List values) = "[" ++ intercalate ", " (map show values) ++ "]"
   show (Thunk force'f)
-    = let val = force'f ()
-      in show val
+    = case force'f () of
+        Left err -> show err
+        Right val -> show val
+
+
+data EvaluationError
+  = UnboundVar String
+  | BadOperatorApplication String Value
+  | IndexOutOfBound Int
+  | NilHeadException
+  | NilTailException
+  | EmptyStringException
+  | DivisionByZero Int
+  | Unexpected String
+
+instance Show EvaluationError where
+  show (UnboundVar name) = "Unknown variable " ++ name
+  show (BadOperatorApplication name exp) = "Bad use of the operator " ++ name ++ "\n  in the expression \n    (" ++ name ++ show exp ++ ")"
+  show (IndexOutOfBound ind) = "Index out of the bound error. (" ++ show ind ++ ")"
+  show NilHeadException = "Native function #head called on an empty list."
+  show NilTailException = "Native function #tail called on an empty list."
+  show EmptyStringException = "Operation called with an empty string."
+  show (DivisionByZero left) = "Division by zero error. (" ++ show left ++ " / 0)"
+  show (Unexpected message) = "Unexpected: " ++ message
