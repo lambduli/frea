5bc13e7339af52b81c9769a480a87a2fe438f0a9
Taskkill <johny95wil@gmail.com>
2021-03-21 19:33:08 +0100

    use type constants instead of in-place hardcoded ones

--- test/InferenceSpec.hs
+++ test/InferenceSpec.hs
@@ -10,13 +10,14 @@ import Compiler.TypeChecker.TypeError
 import Compiler.Syntax.Literal
 
 import Compiler.TypeChecker.Inference (typeof)
+import Compiler.TypeChecker.Type
 
 
 spec :: Spec
 spec = describe "Test the inference" $ do
 
   it "Infers the type of a single integer" $
-     typeof (Lit (LitInt 23)) `shouldBe` Right (ForAll [] $ TyCon "Int")
+    typeof (Lit (LitInt 23)) `shouldBe` Right (ForAll [] t'Int)
 
   it "Infers the type of an empty list" $
     typeof (List []) `shouldBe` Right (ForAll ["a"] $ TyList (TyVar "a"))
@@ -24,32 +25,32 @@ spec = describe "Test the inference" $ do
   it "Infers the type of a tuple" $
     typeof (Tuple [Lit (LitInt 23), Lit (LitBool True), Lit (LitChar 'a')])
     `shouldBe`
-    Right (ForAll [] $ TyTuple [TyCon "Int", TyCon "Bool", TyCon "Char"])
+    Right (ForAll [] $ TyTuple [t'Int, t'Bool, t'Char])
 
   it "Infers the type of a let inside lambda" $
-    "\\ x -> let y = ((#+) (x, 1)) in y" <::> ForAll [] (TyArr (TyCon "Int") (TyCon "Int"))
+    "\\ x -> let y = ((#+) (x, 1)) in y" <::> ForAll [] (TyArr t'Int t'Int)
 
   it "Infers the type of a let inside lambda [prefix] ((+) x 1)" $
-    "\\ x -> let (+) = (\\ a b -> ((#+) (a, b))) y = ((+) x 1) in y" <::> ForAll [] (TyArr (TyCon "Int") (TyCon "Int"))
+    "\\ x -> let (+) = (\\ a b -> ((#+) (a, b))) y = ((+) x 1) in y" <::> ForAll [] (TyArr t'Int t'Int)
 
   it "Infers the type of a let inside lambda [infix] (x + 1)" $
-    "\\ x -> let (+) = (\\ a b -> ((#+) (a, b))) y = (x + 1) in y" <::> ForAll [] (TyArr (TyCon "Int") (TyCon "Int"))
+    "\\ x -> let (+) = (\\ a b -> ((#+) (a, b))) y = (x + 1) in y" <::> ForAll [] (TyArr t'Int t'Int)
 
   it "Infers the type of an equality check (on Int) inside the lambda" $
-    "(\\ x -> if ((#=) (x, 23)) then #t else #f)" <::> ForAll [] (TyArr (TyCon "Int") (TyCon "Bool"))
+    "(\\ x -> if ((#=) (x, 23)) then #t else #f)" <::> ForAll [] (TyArr t'Int t'Bool)
 
   it "Infers the type of polymorphic equality check inside the lambda" $
-    "(\\ x y -> ((#=) (x, y)))" <::> ForAll ["a"] (TyVar "a" `TyArr` (TyVar "a" `TyArr` TyCon "Bool"))
+    "(\\ x y -> ((#=) (x, y)))" <::> ForAll ["a"] (TyVar "a" `TyArr` (TyVar "a" `TyArr` t'Bool))
 
   it "Infers the type of " $
-    "(\\ x -> (1, x))" <::> ForAll ["a"] (TyVar "a" `TyArr` TyTuple [TyCon "Int", TyVar "a"])
+    "(\\ x -> (1, x))" <::> ForAll ["a"] (TyVar "a" `TyArr` TyTuple [t'Int, TyVar "a"])
 
   it "Infers the type of a list of applications" $
-    "let fn = (\\ i -> i) in [(fn 23), (fn ((#+) (23, 1))), (fn 42)]" <::> ForAll [] (TyList $ TyCon "Int")
+    "let fn = (\\ i -> i) in [(fn 23), (fn ((#+) (23, 1))), (fn 42)]" <::> ForAll [] (TyList t'Int)
 
   -- TODO: add more test with infix operators and functions 
   it "Infers the type of a let with infix function expression" $
-    "let plus = (\\ a b -> ((#+) (a, b))) in (23 `plus` 42)" <::> ForAll [] (TyCon "Int")
+    "let plus = (\\ a b -> ((#+) (a, b))) in (23 `plus` 42)" <::> ForAll [] t'Int
 
 
 infix 4 <::>
