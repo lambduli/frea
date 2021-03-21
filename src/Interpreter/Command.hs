10c2c72a711796f001ca6af8a1fd2a3cf0dd1d95
Taskkill <johny95wil@gmail.com>
2021-03-21 19:31:21 +0100

    add Data to Command definition

--- src/Interpreter/Command.hs
+++ src/Interpreter/Command.hs
@@ -1,7 +1,15 @@
 module Interpreter.Command where
 
 import Compiler.Syntax.Expression
+import Compiler.Syntax (Type(..), Scheme(..))
 
 
-newtype Command = Assume [(String, Expression)]
+data Command
+  = Define [(String, Expression)]
+  | Data String [Constr]
+    deriving (Show)
+
+
+data Constr
+  = Con String [Type]
   deriving (Show)
