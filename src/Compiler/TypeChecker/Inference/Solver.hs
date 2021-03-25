dba206cfba7f92696a75ec4017ae09951e0b01ce
Taskkill <johny95wil@gmail.com>
2021-03-25 15:01:28 +0100

    refactor: get better names

--- src/Compiler/TypeChecker/Inference/Solver.hs
+++ src/Compiler/TypeChecker/Inference/Solver.hs
@@ -20,7 +20,6 @@ type Solve a = ExceptT TypeError Identity a
 type Unifier = (Subst, [Constraint])
 
 
--- Run the constraint solver
 runSolve :: [Constraint] -> Either TypeError Subst
 runSolve constrs = runIdentity $ runExceptT $ solver state
   where state = (empty'subst, constrs)
@@ -32,57 +31,56 @@ solver (subst, constraints) =
   case constraints of
     [] -> return subst
     ((type'l, type'r) : constrs) -> do
-      subst'  <- unifies type'l type'r
-      solver (subst' `compose'subst` subst, apply subst' constrs)
+      subst'  <- type'l `unify` type'r
+      solver (subst' `compose` subst, apply subst' constrs)
 
 
-unifies :: Type -> Type -> Solve Subst
-unifies t1 t2 | t1 == t2 = return empty'subst
-unifies (TyVar v) t = v `bind` t
-unifies t (TyVar v) = v `bind` t
-unifies (TyArr t1 t2) (TyArr t3 t4) = unifyMany [t1, t2] [t3, t4]
-unifies (TyCon name'l) (TyCon name'r)
+unify :: Type -> Type -> Solve Subst
+unify t1 t2 | t1 == t2 = return empty'subst
+unify (TyVar v) t = v `bind` t
+unify t (TyVar v) = v `bind` t
+unify (TyArr t1 t2) (TyArr t3 t4) = unifyMany [t1, t2] [t3, t4]
+unify (TyCon name'l) (TyCon name'r)
   | name'l == name'r = return empty'subst
   | otherwise = throwError $ UnifMismatch name'l name'r
-unifies (TyTuple ts'left) (TyTuple ts'right)
+unify (TyTuple ts'left) (TyTuple ts'right)
   = if length ts'left /= length ts'right
     then throwError $ UnifShapeMismatch (TyTuple ts'left) (TyTuple ts'right)
     else unifyMany ts'left ts'right
-unifies (TyList t'left) (TyList t'right)
-  = unifies t'left t'right
-unifies t1 t2 = throwError $ UnifShapeMismatch t1 t2
+unify (TyList t'left) (TyList t'right)
+  = unify t'left t'right
+unify t1 t2 = throwError $ UnifShapeMismatch t1 t2
 
 
 unifyMany :: [Type] -> [Type] -> Solve Subst
 unifyMany [] [] = return empty'subst
 unifyMany (t'l : ts'l) (t'r : ts'r) = do
-  su1 <- unifies t'l t'r
+  su1 <- t'l `unify` t'r
   su2 <- unifyMany (apply su1 ts'l) (apply su1 ts'r)
-  return (su2 `compose'subst` su1)
+  return (su2 `compose` su1)
 unifyMany t'l t'r = throwError $ UnifCountMismatch t'l t'r
 
 
--- Compose substitutions
-compose'subst :: Subst -> Subst -> Subst
-(Sub sub'l) `compose'subst` (Sub sub'r)
+compose :: Subst -> Subst -> Subst
+(Sub sub'l) `compose` (Sub sub'r)
   = Sub $ Map.map (apply (Sub sub'l)) sub'r `Map.union` sub'l
 
 
-occurs :: String -> Type -> Bool
-occurs name (TyVar varname)
+occurs'in :: String -> Type -> Bool
+name `occurs'in` (TyVar varname)
   = name == varname
-occurs name (TyCon conname)
+name `occurs'in` (TyCon conname)
   = False
-occurs name (TyTuple ts)
-  = any (occurs name) ts
-occurs name (TyList t)
-  = occurs name t
-occurs name (TyArr left right)
-  = occurs name left || occurs name right
+name `occurs'in` (TyTuple ts)
+  = any (name `occurs'in`) ts
+name `occurs'in` (TyList t)
+  = name `occurs'in` t
+name `occurs'in` (TyArr left right)
+  = name `occurs'in` left || name `occurs'in` right
 
 
 bind :: String -> Type -> Solve Subst
 bind varname type'
-  | type' == TyVar varname  = return empty'subst
-  | occurs varname type'    = throwError $ InfiniteType varname type'
-  | otherwise               = return $ Sub $ Map.singleton varname type'
+  | type' == TyVar varname    = return empty'subst
+  | varname `occurs'in` type' = throwError $ InfiniteType varname type'
+  | otherwise                 = return $ Sub $ Map.singleton varname type'
