389de5b378df4aee6c70a708d04d1a28b3799edd
Taskkill <johny95wil@gmail.com>
2021-03-25 15:18:31 +0100

    move some harness stuff out of the Inference module to InferUtils

---
+++ src/Compiler/TypeChecker/Inference/InferUtils.hs
@@ -0,0 +1,95 @@
+module Compiler.TypeChecker.Inference.InferUtils where
+
+
+import qualified Data.Set as Set
+import qualified Data.Map.Strict as Map
+
+import Control.Monad.State
+import Control.Monad.Reader
+import Control.Monad.Except
+
+import Compiler.Syntax.Expression
+import Compiler.Syntax.Type
+import Compiler.TypeChecker.Inference.Substituable
+import Compiler.TypeChecker.Inference.TypeEnv
+import Compiler.TypeChecker.Inference.Infer
+import Compiler.TypeChecker.Inference.InferState
+import Compiler.TypeChecker.TypeError
+
+
+letters :: [String]
+letters = [1..] >>= flip replicateM ['a'..'z']
+
+
+fresh :: Infer Type
+fresh = do
+  InferState { count = counter } <- get
+  put $ InferState { count = counter + 1 }
+  return $ TyVar (letters !! counter)
+
+
+real'fresh :: [String] -> a -> Infer Type
+real'fresh vars var = do
+  InferState { count = counter } <- get
+  put $ InferState { count = counter + 1 }
+  let name = letters !! counter
+  if name `elem` vars
+    then real'fresh vars var
+    else return $ TyVar name
+
+
+extend :: TypeEnv -> (String, Scheme) -> TypeEnv
+extend (Env env) (ty'var, scheme) = Env $ Map.insert ty'var scheme env
+
+
+remove :: TypeEnv -> String -> TypeEnv
+remove (Env env) var = Env (Map.delete var env)
+
+
+put'in'env :: (String, Scheme) -> Infer a -> Infer a
+put'in'env (var, scheme) m = do
+  let scope env = remove env var `extend` (var, scheme)
+  local scope m
+
+
+lookup'env :: String -> Infer Type
+lookup'env var = do
+  (Env env) <- ask
+  case Map.lookup var env of
+    Nothing   ->  throwError $ UnboundVariable var
+    Just s    ->  instantiate s
+
+
+instantiate :: Scheme -> Infer Type
+instantiate (ForAll args type') = do
+  args' <- mapM (real'fresh args) args
+  let subst = Sub $ Map.fromList $ zip args args'
+  return $ apply subst type'
+
+
+closeOver :: Type -> Scheme
+closeOver = normalize . generalize (Env Map.empty)
+
+
+normalize :: Scheme -> Scheme
+normalize (ForAll type'args body) = ForAll (fmap snd ord) (normtype body)
+  where
+    ord = zip (Set.toList . ftv $ body) letters
+
+    normtype (TyArr a b) = TyArr (normtype a) (normtype b)
+    normtype (TyCon a) = TyCon a
+    normtype (TyTuple ts) = TyTuple $ map normtype ts
+    normtype (TyList t) = TyList $ normtype t
+    normtype (TyVar a) =
+      case lookup a ord of
+        Just x -> TyVar x
+        Nothing -> error $ "Type variable " ++ show a ++ " not in the signature."
+
+
+generalize :: TypeEnv -> Type -> Scheme
+generalize env type'
+  = ForAll type'args type'
+    where
+      fvt = ftv type'
+      fve = ftv env
+      type'args = Set.toList $ fvt `Set.difference` fve
