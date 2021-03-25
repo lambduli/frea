29804106d488853769aa45c3fcc4608cd82b9913
Taskkill <johny95wil@gmail.com>
2021-03-25 15:22:01 +0100

    remove commented out import to Utils

--- src/Compiler/TypeChecker/DeclarationCheck.hs
+++ src/Compiler/TypeChecker/DeclarationCheck.hs
@@ -7,7 +7,6 @@ import Compiler.Syntax.Declaration
 import Compiler.Syntax.Type
 import Compiler.TypeChecker.Inference
 import qualified Interpreter.Value as Val
--- import Compiler.TypeChecker.Inference.Utils
 import Compiler.TypeChecker.Inference.Infer
 import Compiler.TypeChecker.Inference.TypeEnv
 
