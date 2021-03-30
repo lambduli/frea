94a89c8b7eec89997f1291f036a694adcfd6797f
Taskkill <johny95wil@gmail.com>
2021-03-30 21:17:14 +0200

    start testing the evaluation

--- test/EvalSpec.hs
+++ test/EvalSpec.hs
@@ -1,4 +1,29 @@
 module EvalSpec where
 
+import Test.Hspec
+import System.Exit
+import Control.Monad.State.Lazy
+import qualified Data.Map.Strict as Map
 
--- TODO: implement
+import Compiler.Parser.Parser (parse'expr)
+import Interpreter.Value
+import Interpreter.Evaluate
+import Compiler.Syntax.Literal
+
+
+spec :: Spec
+spec = describe "Test evaluation of expressions" $ do
+  it "Evaluates simple constant" $ do
+    "1" `evals'to` Lit (LitInt 1)
+
+
+
+evals'to :: String -> Value -> IO ()
+expr `evals'to` val = do
+  case parse'expr expr of
+    Left _ -> exitFailure
+    Right expr -> do
+      let error'or'expr'n'state = runState (force expr Map.empty) Map.empty
+      case error'or'expr'n'state of
+        (Left err, mem') -> exitFailure 
+        (Right expr', mem') -> show expr' `shouldBe` show val
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,6 +3,7 @@ import Test.Hspec
 import qualified ParserSpec
 import qualified ParserDeclSpec
 import qualified InferenceSpec
+import qualified EvalSpec
 
 main :: IO ()
 main = hspec spec
@@ -13,4 +14,5 @@ spec = do
   describe "Parsing test" ParserSpec.spec
   describe "Parsing declarations" ParserDeclSpec.spec
   describe "Inference test" InferenceSpec.spec
+  describe "Evaluation test" EvalSpec.spec
 
