ffd71c497f7b9d93d33c6fbb6bec06457ca06c7b
Taskkill <johny95wil@gmail.com>
2021-04-02 17:52:02 +0200

    clean the commented out code

--- src/Compiler/TypeChecker/Inference/TypeOf.hs
+++ src/Compiler/TypeChecker/Inference/TypeOf.hs
@@ -22,27 +22,6 @@ import Compiler.KindChecker.Inference hiding (infer)
 import Compiler.KindChecker.KindError
 
 
-
--- TODO: this will be gone!
--- infer'env :: [Declaration] -> TypeEnv -> Either TypeError TypeEnv
--- infer'env binds t'env
---   = case sequence eiths of
---       Left t'err -> Left t'err
---       Right pairs -> Right $ Env $ Map.fromList pairs
-
---     where
---       infer'pair :: (TypeEnv, [Either TypeError (String, Scheme)]) -> Declaration -> (TypeEnv, [Either TypeError (String, Scheme)])
---       infer'pair (t'env, eiths) (Binding name exp) = case infer'expression t'env exp of
---         Left t'err -> (t'env, Left t'err : eiths)
---         Right scheme ->
---           let Env env'map = t'env
---           in (Env $ Map.insert name scheme env'map, Right (name, scheme) : eiths)
---       infer'pair acc DataDecl{} = acc
-
---       eiths :: [Either TypeError (String, Scheme)]
---       (t'env', eiths) = foldl infer'pair (t'env, []) binds
-
-
 infer'env :: [Declaration] -> TypeEnv -> Either TypeError TypeEnv
 infer'env binds t'env = do
   let only'funs = filter is'fun binds
@@ -96,9 +75,6 @@ infer'expression env expr = case runInfer env (infer expr) of
 
 infer'many :: [(String, Expression)] -> Infer ([(String, Type)], [Constraint])
 infer'many bindings = do
-  -- do env musim pridat to, ze pro kazdy jmeno bindovany v listu bindings
-  -- zanesu uplne cistej ForAll [] . fresh
-  -- pak muzu ten list bindingu nechat vesele infernout list constraintu
   let names = map fst bindings
       gener name = do ForAll [] <$> fresh
   fresh'vars <- mapM gener names
@@ -118,19 +94,6 @@ infer'many' ((name, expr) : exprs) = do
   -- this should actually work
   -- instantiate should do nothing to the fresh type variable because the ForAll
   -- has an empty list of type parameters
-
-  -- (Env env) <- ask
-  -- case Map.lookup name env of
-  --   Nothing                 -> throwError $ UnboundVariable name -- never really happens
-  --   Just (ForAll _ ty'var)  -> do
-  --     (types, constrs') <- infer'many' exprs
-  --     return (types ++ [(name, type')], (ty'var, type') : constraints ++ constrs')
-  
-  -- co si myslim: tady jsem dostal realnej type vyrazu kerej je bindnutej na name
-  -- mel bych pridat dalsi constraint a to ten, zastupny type pro name (ktery uz ted musi byt v envu)
-  -- se musi constraintnout na ten actual type co jsem prave dostal
-  -- to by realne fakt mohlo umoznit vzajemne rekurzivni bi-directional reference
-
 
 typeof :: Expression -> Either TypeError Scheme
 typeof = infer'expression empty't'env
