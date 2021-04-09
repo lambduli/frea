999b33161472d72c9a3c3b59b7edd247fa5c4e01
Taskkill <johny95wil@gmail.com>
2021-04-09 18:41:35 +0200

    add really shameful hack to pretty print strings and lists

--- src/Interpreter/Print.hs
+++ src/Interpreter/Print.hs
@@ -4,6 +4,7 @@ import Control.Monad.State.Lazy
 import Data.List
 import qualified Data.Map.Strict as Map
 
+import Compiler.Syntax.Literal
 
 import Interpreter.Value
 import Interpreter.Evaluate
@@ -36,10 +37,88 @@ print state = do
             let mem' = Map.insert addr val' mem
             put mem'
             print' val'
-      
+
       print' (Data con'tag []) = return $ Right con'tag
+      
+      {-
+        This is really ugly hack to achieve printing strings as strings and lists as lists
+      -}
+
+      print' (Data ":" [head, tail]) = do
+        h'fced <- force'val head
+        tail'fced <- force'val tail
+        case h'fced of
+          Left err -> return $ Left err
+          Right (Lit (LitChar ch)) ->
+            case tail'fced of
+              Left err -> return $ Left err
+              Right tail'v -> do -- je to teda nejakej dalsi pair char a string
+              -- takze to necham print' nout a concatnu je
+                rest <- print'list tail'v
+                case rest of
+                  Left err -> return $ Left err
+                  Right str -> return $ Right $ '"' : ch : str ++ "\""
+            -- ted vim ze to bude string
+            -- takze muzu jit pro zbytek a vzdycky evaluovat
+          Right h'v -> do -- ted je to pripad, kdyz to sice je list, ale ne charu
+          -- takze to musim osetrit normalne
+            str <- print' h'v
+            case str of
+              Left err -> return $ Left err
+              Right str' -> do
+                let prefix = "["
+                case tail'fced of
+                  Left err -> return $ Left err
+                  Right (Data "[]" []) -> -- tohle je teda konec listu
+                    return $ Right $ prefix ++ str' ++ "]"
+                  Right tail'v -> do -- je to teda nejakej dalsi pari "val" a "list"
+                  -- takze to necham print' nout a concatnu je
+                    rest <- print'list tail'v
+                    case rest of
+                      Left err -> return $ Left err
+                      Right sstr -> return $ Right $ prefix ++ str' ++ ", " ++ sstr
+
       print' (Data con'tag args) = do
         printed'vals <- mapM print' args
         case sequence printed'vals of
           Left err -> return $ Left err
           Right vals -> return $ Right $ "(" ++ con'tag ++ " " ++ unwords vals ++ ")"
+
+      print'list (Data "[]" []) = return $ Right ""
+
+      print'list (Data ":" [head, tail]) = do
+        h'fced <- force'val head
+        tail'fced <- force'val tail
+        case h'fced of
+          Left err -> return $ Left err
+          Right (Lit (LitChar ch)) ->
+            case tail'fced of
+              Left err -> return $ Left err
+              -- Right (Data "[]" []) -> -- tohle je teda konec stringu
+              --   return $ Right [ch]
+              Right tail'v -> do -- je to teda nejakej dalsi pair char a string
+              -- takze to necham print' nout a concatnu je
+                rest <- print'list tail'v
+                case rest of
+                  Left err -> return $ Left err
+                  Right str -> return $ Right $ ch : str
+            -- ted vim ze to bude string
+            -- takze muzu jit pro zbytek a vzdycky evaluovat
+          Right h'v -> do -- ted je to pripad, kdyz to sice je list, ale ne charu
+          -- takze to musim osetrit normalne
+            str <- print' h'v
+            case str of
+              Left err -> return $ Left err
+              Right str' -> do
+                case tail'fced of
+                  Left err -> return $ Left err
+                  Right (Data "[]" []) -> -- tohle je teda konec listu
+                    return $ Right $ str' ++ "]"
+                  Right tail'v -> do -- je to teda nejakej dalsi pari "val" a "list"
+                  -- takze to necham print' nout a concatnu je
+                    rest <- print'list tail'v
+                    case rest of
+                      Left err -> return $ Left err
+                      Right sstr -> return $ Right $ str' ++ ", " ++ sstr
+
+      print'list unknown = return $ Left $ Unexpected $ "Something unexpected happened: " ++ show unknown
